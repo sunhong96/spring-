97d39b45e7682a6dddb517b2a3761cdf340d1736
sunhong96 <tjsghd000000@gmail.com>
2023-07-05 22:03:45 +0900

    s

---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+
+}
+// 어노테이션 만들어주기
+//@MainDiscountPolicy 이런식으로 어노테이션을 만들어줄수있음
+//왠만하면 primary로 해결하는게 좋음
